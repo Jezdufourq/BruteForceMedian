26d2b988aeff2b2cf89b275ea108fd049f802cb7
jezdufourq <jezdufourq@gmail.com>
2019-04-03 10:34:36 +1000

    Third commit: ~What: Completed the implementation of measuring the time of the algorithim. I have also implemented the algorithm. ~Why: To measure the efficency of the algorithm.

--- main.cpp
+++ main.cpp
@@ -7,19 +7,31 @@
 #include <ctime>
 #include <ratio>
 #include <chrono>
+#include <tuple>
 
 using namespace std;
 using namespace std::chrono;
 
 vector<int>Rand(int length, int range);
+tuple<long,long,float>BruteForceMedian(vector<int>A);
 void print(vector<int> const &input);
 
 int main()
 {
     int range = 100;
     int length = 20;
+    vector<int>randArray;
 
-    print(Rand(length,range));
+
+    randArray = Rand(length, range);
+
+    auto [median, basicOp, timeTaken] = BruteForceMedian(randArray);
+
+    cout << "This is the random array" << endl;
+    print(randArray);
+    cout << "This is the output" << endl;
+    cout << median << ',' << basicOp << ',' << timeTaken << endl;
+
     return 0;
 }
 
@@ -44,6 +56,47 @@ vector<int>Rand(int length, int range)
     }
     return random_vector;
 }
+
+
+
+tuple<long,long, float> BruteForceMedian(vector<int> A)
+{
+    int n = A.size();
+    int k = ceil(n/2);
+    int basicOp = 0;
+    tuple<int,int> outputArray;
+    duration<double> timeTaken;
+
+    for (int i = 0; i < (n - 1); i++ )
+    {
+        auto startTime = chrono::high_resolution_clock::now();
+
+        int numSmaller = 0;
+        int numEqual = 0;
+        for(int j = 0; j < (n - 1); j++)
+        {
+            if (A[j] < A[i])
+            {
+                basicOp++;
+                numSmaller++;
+            }
+            else if(A[j] == A[i])
+            {
+                basicOp++;
+                numEqual++;
+            }
+        }
+    if(numSmaller < k and k <= (numSmaller+numEqual))
+    {
+        auto endTime = chrono::high_resolution_clock::now();
+        timeTaken = duration_cast<duration<double>>(endTime - startTime);
+
+        return make_tuple(A[i],basicOp, timeTaken.count());
+    }
+    }
+    return tuple<long, long, float>();
+}
+
 
 
 void print(vector<int> const &input)
